2e6647066dbb658f15e9dcaa125d8ce6b22e7469
Neaz Mahmud <neaz6160@gmail.com>
2022-06-03 15:59:29 +0600

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Faculty, Course, Announcement, Assignment, Submission,Material
+from .models import Student, Faculty, Course
 
 admin.site.register(Student)
 admin.site.register(Faculty)
 admin.site.register(Course)
-admin.site.register(Announcement)
-admin.site.register(Assignment)
-admin.site.register(Submission)
-admin.site.register(Material)
