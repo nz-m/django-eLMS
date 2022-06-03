84416ef7f956aeb50e18cb2ad41ed98401d55036
Neaz Mahmud <neaz6160@gmail.com>
2022-06-03 11:35:55 +0600

    Update admin.py

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import StudentDiscussion, FacultyDiscussion
-
-admin.site.register(StudentDiscussion)
-admin.site.register(FacultyDiscussion)
